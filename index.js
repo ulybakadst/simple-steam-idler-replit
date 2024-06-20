5f543eadaaed53463a6b71ff39cfa762e9fee382
ulybakadst <145228178+ulybakadst@users.noreply.github.com>
2024-06-20 16:26:30 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [322330, 440, 730, 570];  // Enter here AppIDs of the needed games
+var games = [322330, 440, 730, 570, 420530];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
